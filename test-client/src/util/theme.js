de0aadf78fee8c90645d36bb9c364fbba16405e6
WinceTan <wincent.tan2@gmail.com>
2020-07-09 18:00:19 +0800

    Exercise2
    Sign Up and Login forms using ReactJS + Material UI

---
+++ test-client/src/util/theme.js
@@ -0,0 +1,19 @@
+﻿export default {
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#35baf6',
+      dark: '#002884',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#ff9100',
+      dark: '#ba000d',
+      contrastText: '#000000',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+};
